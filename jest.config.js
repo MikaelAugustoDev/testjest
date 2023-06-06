15ef2f840c80717eb86c4768c7351d639eee834d
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-06-06 16:23:16 -0300

    configurando e instalando jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	detectOpenHandles: true,
+};
